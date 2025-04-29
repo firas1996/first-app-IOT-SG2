c76f4d9736f92fb5a721b0f34904ab9e289727fa
firas hamila <firas.hm.1996@gmail.com>
2025-04-29 09:59:30 +0200

    a

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,31 @@
 import { Button, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 
 const Home = () => {
   const navigation = useNavigation();
+  const [imp, setImp] = useState("");
+  const [DATA, setDATA] = useState([]);
+  const addItem = () => {
+    if (imp.trim().length > 0) {
+      setDATA([...DATA, { id: Math.random(), name: imp, isFav: false }]);
+      setImp("");
+    }
+    console.log(DATA);
+  };
+  const updateItem = (id) => {
+    setDATA(
+      DATA.map((item) => {
+        return item.id === id ? { ...item, isFav: !item.isFav } : item;
+      })
+    );
+  };
+  const changeImp = (txt) => {
+    console.log("imp: ", imp);
+    setImp(txt);
+    console.log("txt: ", txt);
+    console.log("imp: ", imp);
+  };
   return (
     <View style={styles.container}>
       <Button
